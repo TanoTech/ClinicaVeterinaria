8e8ae2424e2d84cfb54f723b3f827b31b3b08e09
Gaetano <gaetano@hotmail.com>
2024-03-20 10:30:16 +0100

    fix

---
+++ Models/AnimalTheraphyViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BW5.Models
+{
+    public class AnimalTherapyViewModel
+    {
+        public Animal Animal { get; set; }
+        public List<Therapy> Therapies { get; set; }
+    }
+}
