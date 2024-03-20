540c46603e8265046f69caefcfbb3470102e9fa3
Gaetano <gaetano@hotmail.com>
2024-03-20 09:25:58 +0100

    fix

---
+++ Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace BW5_Vet.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<BW5.Models.VetContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(BW5.Models.VetContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
